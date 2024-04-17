12a4ee4dcb23ee6c330d979e965fd8ec72b91429
vera))) <152393554+Verask0@users.noreply.github.com>
2024-04-17 11:49:26 +0000

    Взяли код с гпт

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
 class Table:
-    # метод-конструктор
-    def __init__(self):
-        self.field = []
-        self.col_names = []
+  def __init__(self):
+      self.data = []
 
-    # метод, который добавляет строки в наш объект
-    def create_row(self, d):
-        self.col_names.append(d.keys())
-        self.field.append(d)
+  def add_row(self, row):
+      self.data.append(row)
 
+  def load_from_dicts(self, *dicts):
+      for d in dicts:
+          self.add_row(d)
 
-# как создать экземпляр по классу?
-table1 = Table()
-print(table1.field)
-d = {"cat": "meow", "dog": "woof"}
-table1.create_row(d)
-print(table1.field, table1.col_names)
+  def load_from_file(self, filename):
+      with open(filename, 'r') as file:
+          lines = file.readlines()
+          headers = lines[0].strip().split(',')
+          for line in lines[1:]:
+              row_values = line.strip().split(',')
+              row = dict(zip(headers, row_values))
+              self.add_row(row)
+
+  def __str__(self):
+      if not self.data:
+          return "False"
+      columns = list(self.data[0].keys())
+      max_column_widths = {column: len(column) for column in columns}
+      for row in self.data:
+          for column, value in row.items():
+              max_column_widths[column] = max(max_column_widths[column], len(str(value)))
+      table_str = ""
+      for column in columns:
+          table_str += column.ljust(max_column_widths[column] + 2)
+      table_str += "\n"
+      for row in self.data:
+          for column, width in max_column_widths.items():
+              table_str += str(row.get(column, "")).ljust(width + 2)
+          table_str += "\n"
+      return table_str
+
+# Пример использования:
+fLine = {"a": 2, "b": 4}
+sLine = {"a": 3, "b": 9}
+
+table = Table()
+table.add_row(fLine)
+table.add_row(sLine)
+
+print(table)
+
+table_from_file=Table()
+table_from_file.load_from_file("table.csv")
+print(table_from_file)
+
 
 # Вариант 2
 # Формат csv - универсальный формат таблицы.
@@ -31,8 +65,8 @@ print(table1.field, table1.col_names)
 
 #  Пример
 # Даны словари в качестве строк
-fLine = {"a" : 2, "b" : 4}
-sLine = {"a" : 3, "b" : 9}
+# fLine = {"a" : 2, "b" : 4}
+# sLine = {"a" : 3, "b" : 9}
 
 # Результат при выводе таблицы
 # а  b
